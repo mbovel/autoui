21362ac7f35266409e45cf7eba0316ee12e0b783
Matthieu Bovel <matthieu@bovel.net>
2019-11-13 14:06:21 +0100

    Fix demo

--- src/backends/AutomergeStore.ts
+++ src/backends/AutomergeStore.ts
@@ -10,9 +10,9 @@ export function AutomergeStore<S>(initialState: Automerge.Doc<S>): SimpleStore<S
 function reduce<S>(state: Automerge.Doc<S>, action: UIAction): Automerge.Doc<S> {
 	switch (action.type) {
 		case "set":
-			return Automerge.change(state, (doc: Automerge.Proxy<S>) =>
-				set(doc as object, action.path, action.value)
-			);
+			return Automerge.change(state, (doc: Automerge.Proxy<S>) => {
+				set(doc as object, action.path, action.value);
+			});
 		case "undo":
 			return Automerge.undo(state);
 		case "redo":
--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,31 @@
 import { Auto } from "./view/Auto";
+import * as Automerge from "automerge";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { AutomergeStore } from "./backends/AutomergeStore";
 import { jsonToUI } from "./model/jsonToUI";
 import { withMethods } from "./model/withMethods";
 
-const store = AutomergeStore({
-	firstname: "Matthieu",
-	lastname: "Bovel",
-	details: {
-		email: "mbovel@me.com"
-	}
-});
+const store = AutomergeStore(
+	Automerge.from({
+		firstname: "Matthieu",
+		lastname: "Bovel",
+		details: {
+			email: "mbovel@me.com"
+		}
+	})
+);
 
 function App() {
 	const toUI = jsonToUI();
 	const [ui, setUI] = React.useState(store.getState());
 	React.useEffect(() => {
-		return store.subscribe(() => setUI);
+		return store.subscribe(() => setUI(store.getState()));
 	}, []);
 
 	return <Auto {...withMethods(toUI(ui), store)} />;
 }
 
 ReactDOM.render(<App />, document.body);
+
+export const test = "FOO";
